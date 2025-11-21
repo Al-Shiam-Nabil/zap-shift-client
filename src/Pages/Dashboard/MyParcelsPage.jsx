2749bb3d038fe88f96509b9bc2847e561cfbf77c
Al Shiam Nabil <nabil15-4777@diu.edu.bd>
2025-11-21 18:58:10 +0600

    dashboard and tanstack data load

--- src/Pages/Dashboard/MyParcelsPage.jsx
+++ src/Pages/Dashboard/MyParcelsPage.jsx
@@ -1,6 +1,21 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
+import useAuthHook from '../../Hooks/useAuthHook';
+import useAxiosSequre from '../../Hooks/useAxiosSequre';
 
 const MyParcelsPage = () => {
+const {user}=useAuthHook()
+const sequreAxios=useAxiosSequre() 
+    const {data}=useQuery({
+        queryKey:['myParcel', user?.email],
+        queryFn:async()=>{
+            const result=sequreAxios.get(`/parcels?email=${user?.email}`)
+            return result
+        }
+    })
+
+    console.log(data?.data)
+
     return (
         <div>
             my parcels page
