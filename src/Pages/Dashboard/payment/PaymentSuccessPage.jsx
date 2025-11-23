283fe30c903b1169c100cf5a1bf5bac89607317e
Al Shiam Nabil <nabil15-4777@diu.edu.bd>
2025-11-23 12:18:21 +0600

    transation id and trackig id added

--- src/Pages/Dashboard/payment/PaymentSuccessPage.jsx
+++ src/Pages/Dashboard/payment/PaymentSuccessPage.jsx
@@ -1,21 +1,32 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useSearchParams } from "react-router";
 import useAxiosSequre from "../../../Hooks/useAxiosSequre";
 
 const PaymentSuccessPage = () => {
   const [searchParams] = useSearchParams();
+  const [paymentInfo, setPaymentInfo] = useState({});
   const sessionId = searchParams.get("session_id");
   const sequreAxios = useAxiosSequre();
 
   useEffect(() => {
-    if(sessionId){
-        sequreAxios.patch(`/payment-success/${sessionId}`).then((res) => {
-      console.log(res.data);
-    });
+    if (sessionId) {
+      sequreAxios.patch(`/payment-success/${sessionId}`).then((res) => {
+        console.log(res.data);
+        setPaymentInfo({
+          transactionId: res.data.transactionId,
+          trackingId: res.data.trackingId,
+        });
+      });
     }
   }, [sequreAxios, sessionId]);
 
-  return <div>payment success</div>;
+  return (
+    <div>
+      <h3>Payment success.</h3>
+      <p>Your transaction id : {paymentInfo.transactionId}</p>
+      <p>Your trackingId : {paymentInfo.trackingId}</p>
+    </div>
+  );
 };
 
 export default PaymentSuccessPage;
